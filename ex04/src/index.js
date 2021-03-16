3352332c1451d642f1b5082ede7bb255d4ce15dc
Elvira-o <elvira2021ot@gmail.com>
2021-03-16 01:11:15 +0100

    modified ex04

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -3,7 +3,6 @@ function localScope() {
   console.log(myVariable);
 }
 localScope();
-console.log(myVariable);
 if (typeof myVariable != "undefined") {
   console.log("outside localScope", myVariable);
 } else {
